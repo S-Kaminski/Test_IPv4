41af32888d9c6cf65b793c8890804879574cd121
S-Kaminski <szy.kaminski@gmail.com>
2025-02-14 01:35:56 +0100

    Add valid and invalid test cases for del function

--- test_IPv4.py
+++ test_IPv4.py
@@ -24,7 +24,7 @@ def ip_to_int(ip_str):
     parts = list(map(int, ip_str.split(".")))  # split string by dot into 4
     return (parts[0] << 24) | (parts[1] << 16) | (parts[2] << 8) | parts[3]
 
-
+#fixtures
 @pytest.fixture
 def empty_ipv4():
     for base in range(64):  # Try clearing possible entries
@@ -39,18 +39,17 @@ def ipv4_with_example():
     ipv4.add(base, mask)
     return ipv4, base, mask  # Return the test values
 
-## 
-
+#add test cases
 def test_add_single_prefix_valid(empty_ipv4):
     base = ip_to_int("10.20.0.0")
     mask = 16
     result = empty_ipv4.add(base,mask)
     assert result == 0, f"add(base,mask) function valid test failed - function returned {result}"
 
-def test_add_prefix_with_smallest_mask_valid():
+def test_add_prefix_with_smallest_mask_valid(): # /32
     pass
 
-def test_add_prefix_with_largest_mask_valid():
+def test_add_prefix_with_largest_mask_valid(): # /0
     pass
 
 def test_add_multiple_valid_prefixes_valid():
@@ -59,7 +58,10 @@ def test_add_multiple_valid_prefixes_valid():
 def test_add_base_greater_than_255_invalid():
     pass
 
-def test_add_negative_base_invalid():
+def test_add_prefix_with_negative_base_invalid():
+    pass
+
+def test_add_already_existing_prefix_invalid():
     pass
 
 def test_add_mask_outside_of_range_invalid(empty_ipv4):
@@ -67,4 +69,28 @@ def test_add_mask_outside_of_range_invalid(empty_ipv4):
     mask = 33  #invalid mask (valid range: 0-32)
     result = empty_ipv4.add(base,mask)
     assert result == -1, f"add(base,mask) function invalid test failed - function returned {result}"
+
+#del test cases
+def test_del_existing_prefix_valid():
+    pass
+
+def test_del_existing_smallest_mask_prefix_valid(): # /32
+    pass
+
+def test_del_existing_largest_mask_prefix_valid(): # /0
+    pass
+
+def test_del_nonexisting_prefix_invalid():
+    pass
+
+def test_del_prefix_with_out_of_range_base_invalid():
+    pass
+
+def test_del_prefix_with_out_of_range_mask_invalid():
+    pass
+
+def test_del_prefix_with_negative_base_invalid():
+    pass
+
+
 
